d30546600190dd32b992a028ef7a023f71d867e0
Elliot Lehman <elliotmlehman@protonmail.com>
2020-03-18 01:35:05 -0700

    last of night

--- with_sklearn_cleaning.py
+++ with_sklearn_cleaning.py
@@ -52,13 +52,19 @@ measurement_blacklist = ['cup', 'cups', 'ml', 'tablespoon', 'tablespoons',
 all_ingredient_names = []
 ingredients = []
 titles = []
+title_lengths = []
+
 i = 0
 for key in data:  # iterate through every recipe!
     ing_temp = ''
     for ingredient in data[key]['ingredients']:
         ing_temp += ingredient + ' '
-    titles.append(data[key]['title'])
+
+    title = data[key]['title']
+    titles.append(title)
     ingredients.append(ing_temp)
+    title_length = len(title)
+    title_lengths.append(title_length)
     i += 1
     if i == 50:
         break
@@ -88,21 +94,26 @@ y_train = vectorizer_title.fit_transform(titles_train).toarray()
 y_test = vectorizer_title.fit_transform(titles_test).toarray()
 
 first = True
+
 for row in np.arange(np.shape(x_train)[0]):
+    # Define vectors relating to words, word frequencies, grammar, layout, etc...
+
+    # x corresponds to the word megavector
     x = np.array(x_train[row].transpose())  # Transpose so we have a column to multiply.
     x = x.reshape(np.size(x), 1)  # Reshape for matrix multiplication
     y = np.array(y_train[row])
     y = y.reshape(1, np.size(y))
+    y2 = np.array(title_lengths)
+    y2 = y2.reshape(1, np.size(y2))
 
     if first:
         occurrence_matrix = x * y  # Initial occurrences
+        title_word_number_occurrence_matrix = x * y2
         first = False
     else:
         occurrence_matrix += x * y  # Sum up occurrences.
+        title_word_number_occurrence_matrix += x * y2  # Sum up occurrences.
 
-# max_indices = np.unravel_index(np.argpartition(occurrence_matrix, -5, axis=None)[-5:], occurrence_matrix.shape)
-# print(max_indices[1][1])
-max_index = np.unravel_index(np.argmax(occurrence_matrix, axis=None), occurrence_matrix.shape)
 
 title_word_blacklist_indices = []
 recipe_word_blacklist_indices = []
@@ -116,8 +127,6 @@ for word in all_ingredient_words:
         #  Get indices of words that are not nouns or are measurements.
         recipe_word_blacklist_indices.append(all_ingredient_words.index(word))
 
-for index in recipe_word_blacklist_indices:
-    occurrence_matrix[index, :] = 0  # zero out any row that is blacklisted
 
 for word in all_title_words:
     # loop through all words, and if they are on the blacklist, add their indices to the blacklist indices list
@@ -129,19 +138,49 @@ for word in all_title_words:
         #  Get indices of words that are not nouns or are measurements.
         title_word_blacklist_indices.append(all_title_words.index(word))  # append those to the blacklist list
 
-om = copy.deepcopy(occurrence_matrix)  # create a copy so things can be deleted and not lose data from the main list
-max_indices = []
-for i in np.arange(10):  # this loop finds max index values in the occurrence matrix
-    temp_max_index = np.unravel_index(np.argmax(om, axis=None), om.shape)
-    max_indices.append(temp_max_index)
-    print(om[temp_max_index])
-    om[temp_max_index] = 0  # set this value to zero now so that it doesn't get detected as a maximum
+occurrence_matrix_blacklisted = copy.deepcopy(occurrence_matrix)  # define a matrix that will be corrected by blacklist
 
-print(max_indices)
+for index in recipe_word_blacklist_indices:
+    occurrence_matrix_blacklisted[index, :] = 0  # zero out any row that is blacklisted
+
+for index in title_word_blacklist_indices:
+    occurrence_matrix_blacklisted[:, index] = 0  # zero out any row that is blacklisted
+
+# create a copy so things can be deleted and not lose data from the main list
+omb = copy.deepcopy(occurrence_matrix_blacklisted)
+
+
+'''store the indices of the matrix corrsponding to the ingredient word (row) and title word (col) that
+occur the most frequently together'''
+max_indices_ing_title = []
+
+# same as above but ingredient word and number of words in the title (the length)
+max_indices_ing_titlelength = []
+
+number_of_maxes = 10  # the number of maxes to find and store in `max_indices`
+for i in np.arange(10):  # this loop finds max index values in the occurrence matrix
+    max_index = np.unravel_index(np.argmax(omb, axis=None), omb.shape)
+    max_indices_ing_title.append(max_index)
+    print(omb[max_index])
+    omb[max_index] = 0  # set this value to zero now so that it doesn't get detected as a maximum
+
+print(max_indices_ing_title)
 print('testing')
 # print(om[max_index])
 # print(max_index)
-for index in max_indices:
+for index in max_indices_ing_title:
     ing = all_ingredient_words[index[0]]
     title = all_title_words[index[1]]
     print(ing, '&', title, r'\\')
+
+# NEED TO CREATE CORRELATION BETWEEN INGREDIENT WORDS AND TITLE LENGTH (NUMBER OF WORDS)
+
+
+def distance_score(string1, string2):
+    word_difference = list(set(string1) - set(string2))
+    score = len(word_difference)
+    return score
+
+
+
+
