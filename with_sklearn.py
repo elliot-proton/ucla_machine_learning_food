e99e918387acbf55e3bdd873073116fc01621f28
Elliot Lehman <elliotmlehman@protonmail.com>
2020-03-17 21:55:15 -0700

    Latex update.
    Added highest related word count index extraction to `with_sklearn.py`

--- with_sklearn.py
+++ with_sklearn.py
@@ -1,3 +1,4 @@
+import copy
 import json  # for dealing with json files
 import re  # regular expression library
 import pandas as pd
@@ -56,14 +57,17 @@ ing_test = ingredients[40:]  # ingredients test set
 
 v_ing = vectorizer_ing.fit_transform(ing_train)
 v_title = vectorizer_title.fit_transform(titles_train)
-print(vectorizer_ing.get_feature_names())
-print(v_ing.toarray())
+all_ingredient_words = vectorizer_ing.get_feature_names()
+all_title_words = vectorizer_title.get_feature_names()
+
+# print(vectorizer_ing.get_feature_names())
+# print(v_ing.toarray().shape)
+
 x_train = vectorizer_ing.fit_transform(ing_train).toarray()
 x_test = vectorizer_ing.fit_transform(ing_test).toarray()
 y_train = vectorizer_title.fit_transform(titles_train).toarray()
 y_test = vectorizer_title.fit_transform(titles_test).toarray()
-print(np.shape(x_train))
-print(np.shape(y_train))
+
 first = True
 for row in np.arange(np.shape(x_train)[0]):
     x = np.array(x_train[row].transpose())  # Transpose so we have a column to multiply.
@@ -77,7 +81,24 @@ for row in np.arange(np.shape(x_train)[0]):
     else:
         occurrence_matrix += x * y          # Sum up occurrences.
 
-print(np.shape(occurrence_matrix))
-print(np.max(occurrence_matrix))
+#max_indices = np.unravel_index(np.argpartition(occurrence_matrix, -5, axis=None)[-5:], occurrence_matrix.shape)
+#print(max_indices[1][1])
 max_index = np.unravel_index(np.argmax(occurrence_matrix, axis=None), occurrence_matrix.shape)
-print(max_index)
+
+om = copy.deepcopy(occurrence_matrix)
+max_indices = []
+for i in np.arange(10): # this loop finds max index values in the occurrence matrix
+    temp_max_index = np.unravel_index(np.argmax(om, axis=None), om.shape)
+    max_indices.append(temp_max_index)
+    print(om[temp_max_index])
+    om[temp_max_index] = 0  # set this value to zero now so that it doesn't get detected as a maximum
+
+print(max_indices)
+print('testing')
+#print(om[max_index])
+#print(max_index)
+for index in max_indices:
+    print(index)
+    ing = all_ingredient_words[index[0]]
+    title = all_title_words[index[1]]
+    print(ing, title)
